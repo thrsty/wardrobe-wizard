53d6a7fac7445bc9853e4eff86fc3312b33d97e8
ed hirst <edthirst@gmail.com>
2023-09-11 10:43:03 +1000

    init commit

---
+++ wardrobe-wizard/Models/Clothes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using SQLite;
+
+namespace wardrobe_wizard.Models
+{
+	public class Clothes
+	{
+		[PrimaryKey, AutoIncrement]
+
+		public string name { get; set; }
+        public int id { get; set; }
+        public string color { get; set; }
+        public string type { get; set; }
+        public Image Image { get; set; }
+        public string brand { get; set; }
+        public string fit { get; set; }
+        public string material { get; set; }
+        public int price { get; set; }
+        public string formality { get; set; }
+        public bool isClean { get; set; }
+    }
+}
+
---
+++ wardrobe-wizard/Services/ClothesDatabase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using wardrobe_wizard.Models;
+using SQLite;
+
+namespace wardrobe_wizard.Services
+{
+	public class ClothesDatabase
+	{
+		static SQLiteAsyncConnection db;
+
+		static async Task Init()
+		{
+			if (db != null)
+				return;
+
+			// Gets absolute path to the database file
+			var databasePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "ClothesDatabase.db");
+			db = new SQLiteAsyncConnection(databasePath);
+		}
+	}
+}
+
