890c5fb6d2bbe1185023890df8b29633dacda66e
ed hirst <edthirst@gmail.com>
2023-10-09 10:35:44 +1100

    fixed conflict?

--- wardrobe-wizard/newItem.xaml.cs
+++ wardrobe-wizard/newItem.xaml.cs
@@ -14,7 +14,7 @@ public partial class newItem : ContentPage
         // Adds different types of clothing to picker for user to pick from
         var typeList = new List<string>()
         {
-            "Top", "Bottom", "Jacket", "Shoes", "Socks", "Hat", "Accessory"
+            "T-shirt", "Long sleeved t-shirt", "Pants", "Shorts", "Jacket", "Shoes", "Socks", "Hat", "Ring/s", "Necklace"
         };
         typePicker.ItemsSource = typeList;
 
